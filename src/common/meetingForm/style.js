0eaa460b0020d5d1f37d7131cd953ca0e4095b6b
Donard97 <donardgolaj@gmail.com>
2022-02-01 13:54:53 +0100

    Add style of form to seperate file

---
+++ src/common/meetingForm/style.js
@@ -0,0 +1,34 @@
+const style = {
+
+  form: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '60%',
+    bgcolor: 'whitesmoke',
+    border: '1px solid white',
+    borderRadius: 7,
+    boxShadow: 24,
+    p: 4,
+    margin: 'auto',
+  },
+
+  header: {
+    textAlign: 'center',
+    p: 2,
+    fontSize: '1.5rem',
+    fontWeight: 600,
+    letterSpacing: '1px',
+  },
+
+  btn: {
+    display: 'flex',
+    margin: 'auto',
+    bgcolor: 'teal',
+    width: '100%',
+    padding: '10px 0',
+  },
+};
+
+export default style;
