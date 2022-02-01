5c67deaf1f140a5905bacf10db27c3980a105b6c
ahzia <ah.ziayosfi@gmail.com>
2022-02-02 02:11:32 +0430

    add user stat to redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../features/user/userSlice';
+import snackbarReducer from '../common/snackbar/snackbarSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
+    snackbar: snackbarReducer,
   },
 });
 
