ea8d496bf9b40a9854b52b7beeac7030994941ea
ahzia <ah.ziayosfi@gmail.com>
2022-02-02 02:09:36 +0430

    finalize function to login and register throw API

--- src/api/user.js
+++ src/api/user.js
@@ -7,19 +7,19 @@ const endPoinLoginOrRegister = `${endPointUser}/login-register`;
 const loginOrRegister = async (userName) => new Promise((resolve, reject) => {
   fetch(`${endPoinLoginOrRegister}?user_name=${userName}`)
     .then((response) => {
-      if (response.status === 200) {
+      if (response.status === 200 || response.status === 201) {
         response.json().then((data) => {
           resolve(data);
         });
       } else {
         // Backend Error Handing Using Status and Error Message
         response.json().then((data) => {
-          console.log(data);
+          reject(new Error(`${data.error}: ${data.message}`));
         });
       }
     })
     .catch((error) => {
-      reject(error);
+      reject(new Error(`Server Error: ${error.message}`));
     });
 });
 
