01f788f9cf496eb6e7ee7d7496ec3fbdc4c9f836
ahzia <ah.ziayosfi@gmail.com>
2022-02-03 00:46:16 +0430

    write API request for user reservations

---
+++ src/api/reservation.js
@@ -0,0 +1,24 @@
+import baseURL from '../app/api';
+
+const endPointUser = `${baseURL}/users`;
+
+const userReservedMeetings = async (userId) => new Promise((resolve, reject) => {
+  fetch(`${endPointUser}/${userId}/reservations`)
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        response.json().then((data) => {
+          resolve(data);
+        });
+      } else {
+        response.json().then((data) => {
+          reject(new Error(`${data.error}: ${data.message}`));
+        });
+      }
+    })
+    .catch((error) => {
+      reject(new Error(`Server Error: ${error.message}`));
+    });
+});
+
+
+export {userReservedMeetings};
