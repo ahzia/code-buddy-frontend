823edaa25bf2babbc531e46a7fdbc633f05e65a7
ahzia <ah.ziayosfi@gmail.com>
2022-02-02 23:17:01 +0430

    write API request to get user meetings

--- src/api/user.js
+++ src/api/user.js
@@ -1,18 +1,17 @@
 import baseURL from '../app/api';
 
 const endPointUser = `${baseURL}/users`;
-// const endPointUserDetails = `${baseURL}/user`;
-const endPoinLoginOrRegister = `${endPointUser}/login-register`;
+// const endPointUserDetails = `${endPointUser}/user_details`;
+
 
 const loginOrRegister = async (userName) => new Promise((resolve, reject) => {
-  fetch(`${endPoinLoginOrRegister}?user_name=${userName}`)
+  fetch(`${endPointUser}/login-register?user_name=${userName}`)
     .then((response) => {
       if (response.status === 200 || response.status === 201) {
         response.json().then((data) => {
           resolve(data);
         });
       } else {
-        // Backend Error Handing Using Status and Error Message
         response.json().then((data) => {
           reject(new Error(`${data.error}: ${data.message}`));
         });
@@ -23,4 +22,23 @@ const loginOrRegister = async (userName) => new Promise((resolve, reject) => {
     });
 });
 
-export default (loginOrRegister);
+const userMeetings = async (userId) => new Promise((resolve, reject) => {
+  fetch(`${endPointUser}/${userId}/meetings`)
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        response.json().then((data) => {
+          resolve(data);
+        });
+      } else {
+        response.json().then((data) => {
+          reject(new Error(`${data.error}: ${data.message}`));
+        });
+      }
+    })
+    .catch((error) => {
+      reject(new Error(`Server Error: ${error.message}`));
+    });
+});
+
+
+export {loginOrRegister, userMeetings};
