844acfddad483ef17e201f5baa75700a79429776
ahzia <ah.ziayosfi@gmail.com>
2022-02-03 12:04:05 +0430

    add meetingReduser to Redux store

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import userReducer from '../features/user/userSlice';
 import snackbarReducer from '../common/snackbar/snackbarSlice';
+import meetingReducer from '../features/meeting/meetingSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     user: userReducer,
     snackbar: snackbarReducer,
+    meeting: meetingReducer,
   },
 });
 
