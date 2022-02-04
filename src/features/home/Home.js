afb4780c92e522ed5d1400faf4c191351bb142ec
ahzia <ah.ziayosfi@gmail.com>
2022-02-04 19:08:09 +0430

    show all meetings from redux store

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,51 +1,41 @@
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import MeetingCard from '../../common/meetingCard/MeetingCard';
+import { selectAllMeetingsState, allMeetingsAsync } from '../meeting/meetingSlice';
 // import MeetingForm from '../../meetingForm/MeetingForm';
 
-const meeting = {
-  meeting_id: 1,
-  meeting_title: 'Introduction to CS',
-  description: 'This a demo meeting creatied ...',
-  user_id: 1,
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(allMeetingsAsync());
+  }, []);
+  const meetings = useSelector(selectAllMeetingsState);
+  return (
+    <main>
+      <Box sx={{ paddingTop: '4rem' }}>
+        {/* <MeetingForm /> */}
+      </Box>
+      <Container>
+        <Grid
+          container
+          sx={{
+            gap: '3rem',
+            margin: '4rem 0',
+            justifyContent: 'center',
+          }}
+        >
+          {meetings.map((meeting) => (
+            <Grid key={meeting.id}>
+              <MeetingCard meeting={meeting} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </main>
+  );
 };
-
-const Home = () => (
-  <main>
-    <Box sx={{ paddingTop: '4rem' }}>
-      {/* <MeetingForm /> */}
-    </Box>
-    <Container>
-      <Grid
-        container
-        sx={{
-          gap: '3rem',
-          margin: '4rem 0',
-          justifyContent: 'center',
-        }}
-      >
-        <Grid>
-          <MeetingCard meeting={meeting} />
-        </Grid>
-        <Grid>
-          {/* <MeetingCard /> */}
-        </Grid>
-        <Grid>
-          {/* <MeetingCard /> */}
-        </Grid>
-        <Grid>
-          {/* <MeetingCard /> */}
-        </Grid>
-        <Grid>
-          {/* <MeetingCard /> */}
-        </Grid>
-        <Grid>
-          {/* <MeetingCard /> */}
-        </Grid>
-      </Grid>
-    </Container>
-  </main>
-);
 
 export default Home;
