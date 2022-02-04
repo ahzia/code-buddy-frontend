779700d91c8976a86b93170aa958f21819e752ff
ahzia <ah.ziayosfi@gmail.com>
2022-02-05 01:10:01 +0430

    create reservation component

---
+++ src/features/meeting/Reserved.js
@@ -0,0 +1,36 @@
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import MeetingCard from '../../common/meetingCard/MeetingCard';
+import { selectAllMeetingsState, allMeetingsAsync } from './meetingSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(allMeetingsAsync());
+  }, []);
+  const meetings = useSelector(selectAllMeetingsState);
+  return (
+    <main>
+      <Container>
+        <Grid
+          container
+          sx={{
+            gap: '3rem',
+            margin: '4rem 0',
+            justifyContent: 'center',
+          }}
+        >
+          {meetings.map((meeting) => (
+            <Grid key={meeting.id}>
+              <MeetingCard meeting={meeting} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </main>
+  );
+};
+
+export default Home;
