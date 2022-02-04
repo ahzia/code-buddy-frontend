19d9e36c98fbcc650e3e990ef1ac12349f70b0a1
ahzia <ah.ziayosfi@gmail.com>
2022-02-05 00:59:37 +0430

    write API opst request to create meeting

--- src/api/meeting.js
+++ src/api/meeting.js
@@ -21,4 +21,29 @@ const getAllMeetings = async () => new Promise((resolve, reject) => {
     });
 });
 
-export default getAllMeetings;
+const createMeeting = async (meeting) => new Promise((resolve, reject) => {
+  fetch(endPointMeeting, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(meeting),
+  })
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        response.json().then((data) => {
+          resolve(data);
+        });
+      } else {
+        // Backend Error Handing Using Status and Error Message
+        response.json().then((data) => {
+          reject(new Error(`${data.error}: ${data.message}`));
+        });
+      }
+    })
+    .catch((error) => {
+      reject(new Error(`Server Error: ${error.message}`));
+    });
+});
+
+export { createMeeting, getAllMeetings };
