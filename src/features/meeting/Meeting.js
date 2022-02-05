8c36641d9db920caea9fc7596db64537c1a2fdb5
Donard97 <donardgolaj@gmail.com>
2022-02-05 02:42:26 +0100

    Add functions and content to meetings

---
+++ src/features/meeting/Meeting.js
@@ -0,0 +1,41 @@
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Box from '@mui/material/Box';
+import MeetingCard from '../../common/meetingCard/MeetingCard';
+import { userMeetingsAsync, selectUserMeetingsState } from './meetingSlice';
+import MeetingForm from './meetingForm/MeetingForm';
+
+const Meeting = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(userMeetingsAsync());
+  }, []);
+  const meetings = useSelector(selectUserMeetingsState);
+  return (
+    <main>
+      <Container>
+        <Grid
+          container
+          sx={{
+            gap: '3rem',
+            margin: '4rem 0',
+            justifyContent: 'center',
+          }}
+        >
+          <Box sx={{ paddingTop: '4rem' }}>
+            <MeetingForm />
+          </Box>
+          {meetings.map((meeting) => (
+            <Grid key={meeting.id}>
+              <MeetingCard meeting={meeting} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </main>
+  );
+};
+
+export default Meeting;
